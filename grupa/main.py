4c7ea0d55cf18c0eeb4085b1e2c6bd9c2a244e46
ludvigs4 <144446967+ludvigs4@users.noreply.github.com>
2024-04-30 10:28:07 +0300

    Update main.py

--- grupa/main.py
+++ grupa/main.py
@@ -1,4 +1,17 @@
-def main():
+import random
+
+def macities(prieksmets, tema):
+    try:
+        with open(f"{prieksmets}_{tema}.txt", "r", encoding="utf8") as file:
+            rindas = file.readlines()
+            random_rinda = random.choice(rindas).strip().split(" - ")
+            print(random_rinda[0])
+            input("Spiediet enter lai parādītu atbildi!")
+            print(random_rinda[1])
+    except FileNotFoundError:
+        print("Šāda faila nav!💀💀💀")
+
+def galvena():
     print("sveicināti kartiņu programmā!")
     while True:
         izveles=input("Izvēlaties, ko darīt: \n1.)pievienot kartiņu\n2.)mācīties no kartiņām\n3.)beigt programmu\n izvēle: ")
@@ -8,4 +21,5 @@ def main():
             macities()
         elif izveles=="3":
             print("exit")
-main()
+
+galvena()

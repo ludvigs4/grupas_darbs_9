7e1857b472bfa454c07b38db4b20232c6e85c24b
ludvigs4 <144446967+ludvigs4@users.noreply.github.com>
2024-05-03 11:08:40 +0300

    Add files via upload

--- grupa/main.py
+++ grupa/main.py
@@ -11,17 +11,35 @@ def macities(prieksmets, tema):
     except FileNotFoundError:
         print("Šāda faila nav!💀💀💀")
 
+def izdzest(prieksmets, tema, jautajums, atbilde):
+    kopa = f"{jautajums} - {atbilde}"
+    print(kopa)
+    rindas = []
+    try:
+        with open(f"{prieksmets}_{tema}.txt", "r", encoding="utf8") as file:
+            rindas = file.readlines()
+            rindas[0].split(" - ")
+        with open(f"{prieksmets}_{tema}.txt", "w", encoding="utf8") as file:
+            for i in rindas:
+                if kopa != i:
+                    file.write(i)
+    except FileNotFoundError:
+        print("Šāda faila nav!💀💀💀")
+
 def galvena():
     print("sveicināti kartiņu programmā!")
     while True:
-        izveles=input("Izvēlaties, ko darīt: \n1.)pievienot kartiņu\n2.)mācīties no kartiņām\n3.)beigt programmu\n izvēle: ")
+        izveles=input("Izvēlaties, ko darīt: \n1.)pievienot kartiņu\n2.)mācīties no kartiņām\n3.)dzēst kartīti\n izvēle: ")
         if izveles == "1":
             #pievienot_kartiti()
             pass
         elif izveles == "2":
             macities("fizika", "energija")
         elif izveles == "3":
-            print("Programma beidzas!")
-            exit()
+            prieksmets = input("Ievadiet priekšmetu no, kura vēlaties dzēst: ")
+            tema = input("Ievadiet tēmu no, kuras vēlaties dzēst: ")
+            jaut = input("Ievadiet jautājumu, kura vēlaties dzēst: ")
+            atb = input("Ievadiet atbildi, kuru vēlaties dzēst: ")
+            izdzest(prieksmets, tema, jaut, atb)
 
 galvena()
